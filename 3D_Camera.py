adea12f5d5dbb794e26f86ca593d3ecdbbb88565
lucassett <lucassettergren@gmail.com>
2023-09-27 16:02:28 -0700

    Rotation matrices around each axis working for individual values (yaw/pitch/roll); full extrinsic matrix

--- 3D_Camera.py
+++ 3D_Camera.py
@@ -4,31 +4,97 @@ import math
 import numpy as np
 rad = math.radians
 
-def extrinsic(zx, zy, zz, theta):
-    Rx = np.array([
+def Rx(roll):
+    return np.array([
         [1, 0, 0],
-        [0, np.cos(theta), -np.sin(theta)],
-        [0, np.sin(theta), np.cos(theta)],
+        [0, np.cos(roll), -np.sin(roll)],
+        [0, np.sin(roll), np.cos(roll)],
     ])
-    Ry = np.array([
-        [np.cos(theta), 0, np.sin(theta)],
+
+def Ry(pitch):
+    return np.array([
+        [np.cos(pitch), 0, np.sin(pitch)],
         [0, 1, 0],
-        [-np.sin(theta), 0, np.cos(theta)],
+        [-np.sin(pitch), 0, np.cos(pitch)],
     ])
-    Rz = np.array([
-        [np.cos(theta), -np.sin(theta), 0],
-        [np.sin(theta), np.cos(theta), 0],
+
+def Rz(yaw):
+    return np.array([
+        [np.cos(yaw), -np.sin(yaw), 0],
+        [np.sin(yaw), np.cos(yaw), 0],
         [0, 0, 1],
     ])
-    R = Rx @ Ry @ Rz
-    Rpos = R @ np.array([[zx], [zy], [zz]])
+
+def R(roll, pitch, yaw):
+    rotx = Rx(roll)
+    roty = Ry(pitch)
+    rotz = Rz(yaw)
+    return rotx @ roty @ rotz
+
+def extrinsic(zx, zy, zz, roll, pitch, yaw):
+    '''
+    With 0 rotation, the camera points along the object x-axis, and down is the positive z-axis. The camera rotates:
+    1) around the z axis
+    2) around the y' axis
+    3) around the x'' axis
+    :param zx: x position of camera
+    :param zy: y position of camera
+    :param zz: z position of camera
+    :param theta: angle the camera is rotated
+    :return: Extrinsic matrix in form:
+        [R -Rz]
+        [0 0 0 1]
+        where R is a 3x3 matrix formed from Rx @ Ry @ Rz, and -Rz is a 3x1 vector of position in x, y, and z.
+        Final result: 4x4 extrinsic matrix
+    '''
+    rot_mtx = R(roll, pitch, yaw)
+    pos_vec = rot_mtx @ np.array([[zx], [zy], [zz]])
     ext_mtx = np.block([
-        [R, -Rpos],
+        [rot_mtx, -pos_vec],
         [0, 0, 0, 1],
     ])
-
     return ext_mtx
 
+def intrinsic(focal_length, principal_offset_x, principal_offset_y):
+    return np.array([
+        [focal_length, 0, principal_offset_x],
+        [0, focal_length, principal_offset_y],
+        [0, 0, 1],
+    ])
+def hc(inhomogeneous_coord):
+    return np.append(np.asarray(inhomogeneous_coord), 1)
+
+#Inhomogeneous Coordinates
+def ic(homogeneous_coord):
+    return homogeneous_coord[:-1] / homogeneous_coord[-1]
+
+def CAMERA(cx, cy, cz, r, c, hx, hy):
+    return intrinsic(c, hx, hy) @ np.eye(3,4) @ extrinsic(cx, cy, cz, r)
+
+
+
 if __name__ == '__main__':
-    ext_test = extrinsic(0, 0, 0, rad(0))
+    ext_test = extrinsic(0, 0, 0, rad(0), rad(0), rad(0))
     stophere=1
+
+    # with open('HW3_input.csv') as csv:
+    #     for line in csv:
+    #         #print(line)
+    #         parameters = []
+    #         dpt = line.split(',')       #Returns list of strings
+    #         if dpt[0]=='cam_x':
+    #             continue
+    #
+    #         for element in dpt[:]:
+    #             fdpt = float(element)
+    #             parameters.append(fdpt)
+    #
+    #         cam_x, cam_y, cam_z, yaw, pitch, roll, cam_c, pp_x, pp_y, p_x, p_y, p_z, img_x, img_y = parameters
+    #         #csv_test = CAMERA(cam_x, cam_y, cam_z, 'R', cam_c, pp_x, pp_y)
+    #
+    #         ext_mtx = extrinsic(cam_x, cam_y, cam_z, rad(0))
+    #         img_point = hc([p_x, p_y, p_z])
+    #         test_ext = ext_mtx @ img_point
+    #         reduced = np.eye(3,4) @ test_ext
+    #         unit_cam_ans = ic(reduced)
+            #stophere=1
