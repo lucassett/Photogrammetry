9b86f7aec5c4c088940b314b8fa2d0b24d6435ce
lucassett <lucassettergren@gmail.com>
2023-09-27 11:54:11 -0700

    Beginning 3D extension of 2D camera

---
+++ 3D_Camera.py
@@ -0,0 +1,12 @@
+#Extending the 2D camera into 3D, using matrix form rewrite from 2D_1D_Transformation.py
+
+import math
+import numpy as np
+rad = math.radians
+
+
+
+
+
+if __name__ == '__main__':
+    
